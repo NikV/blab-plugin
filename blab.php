f30b23e55564309d3f7c9ee0b9ec5e25c9eb7efc
NikV <techvoltz@gmail.com>
2015-07-11 00:21:04 -0500

    Docs

--- blab.php
+++ blab.php
@@ -1,8 +1,19 @@
 <?php
 /**
  * Plugin Name: The Blab.im Plugin
+ * Plugin URI:
+ * Description: The unofficial plugin for Blab.im that allows you to embed a user's stream into your WordPress site
+ * Version: 1.0
+ * Author: Nikhil Vimal
+ * Author URI: http://nik.techvoltz.com
+ * License: GPL2
  */
 
+/**
+ * The main Blab shortcode function
+ *
+ * @param array $atts The Shortcode Attributes
+ */
 function blab_shortcode( $atts ) {
 	$a = shortcode_atts( array(
 		'user' => '',
